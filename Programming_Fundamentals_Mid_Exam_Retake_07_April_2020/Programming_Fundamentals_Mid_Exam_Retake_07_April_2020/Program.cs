d1846155929a908ecfb88d6bbda51383f674ae66
Zhivko <fbinnzhivko@gmail.com>
2020-06-17 00:08:19 +0300

    Add 2 exams for max points

---
+++ Programming_Fundamentals_Mid_Exam_Retake_07_April_2020/Programming_Fundamentals_Mid_Exam_Retake_07_April_2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programming_Fundamentals_Mid_Exam_Retake_07_April_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
