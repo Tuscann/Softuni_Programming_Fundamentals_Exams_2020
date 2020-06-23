9beeba477dc03c9f90269337fa23520a2f0b48a7
Zhivko <fbinnzhivko@gmail.com>
2020-06-23 22:18:22 +0300

    Add one more exam on C#.

---
+++ Programming Fundamentals Exam - 10 December 2019/01_Disneyland_Journey/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01_Disneyland_Journey
+{
+    class Program
+    {
+        static void Main()
+        {
+            double priceJourney = double.Parse(Console.ReadLine());
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            double sumTotal = 0;
+
+            double permouth = priceJourney / 4;
+            
+            for (int i = 1; i <= numbersCount; i++)
+            {
+                if (i > 1 && i % 2 != 0)
+                {
+                    sumTotal *= 0.84;
+                }
+
+                if (i % 4 == 0)
+                {
+                    sumTotal *= 1.25;
+                }
+                sumTotal += permouth;
+            }
+
+            if (sumTotal >= priceJourney)
+            {
+                Console.WriteLine("Bravo! You can go to Disneyland and you will have {0:0.00}lv. for souvenirs.", sumTotal - priceJourney);
+            }
+            else
+            {
+                Console.WriteLine("Sorry. You need {0:0.00}lv. more.", priceJourney - sumTotal);
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals Exam - 10 December 2019/03_School_Library/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_School_Library
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> books = Console.ReadLine().Split("&").ToList();
+
+            void Swap(int indexFirst, int indexSecound)
+            {
+                var tmp = books[indexFirst];
+                books[indexFirst] = books[indexSecound];
+                books[indexSecound] = tmp;
+            }
+
+            while (true)
+            {
+                string commands = Console.ReadLine();
+
+                if (commands is "Done")
+                {
+                    for (int i = 0; i < books.Count; i++)
+                    {
+                        if (i < books.Count - 1)
+                        {
+                            Console.Write(books[i] + ", ");
+                        }
+                        else
+                        {
+                            Console.WriteLine(books[i]);
+                        }
+                    }
+                    break;
+                }
+
+                string[] currentRow = commands.Split(" | ").ToArray();
+
+                string command = currentRow[0];
+                string bookname = currentRow[1];
+
+                if (command is "Add Book")
+                {
+                    if (!books.Contains(bookname))
+                    {
+                        books.Insert(0, bookname);
+                    }
+                }
+                else if (command is "Take Book")
+                {
+                    if (books.Contains(bookname))
+                    {
+                        books.Remove(bookname);
+                    }
+                }
+                else if (command is "Swap Books")
+                {
+                    string newBookname = currentRow[2];
+
+                    if (books.Contains(bookname) && books.Contains(newBookname))
+                    {
+                        int indexFirst = books.IndexOf(bookname);
+                        int indexSecound = books.IndexOf(newBookname);
+
+                        Swap(indexFirst, indexSecound);
+                    }
+                }
+                else if (command is "Insert Book")
+                {
+                    books.Insert(books.Count, bookname);
+                }
+                else if (command is "Check Book")
+                {
+                    int index = int.Parse(bookname);
+
+                    if (0 <= index && index < books.Count)
+                    {
+                        Console.WriteLine(books[index]);
+                    }
+                }
+            }
+        }
+    }
+}
