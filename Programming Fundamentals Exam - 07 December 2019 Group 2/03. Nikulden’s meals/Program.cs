c9e88e52f641227d1f8189893c2eccff800442cd
Zhivko <fbinnzhivko@gmail.com>
2020-06-24 21:16:36 +0300

    Add 1 more exam 07.12.2019

---
+++ Programming Fundamentals Exam - 07 December 2019 Group 2/03. Nikulden’s meals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Nikulden_s_meals
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
